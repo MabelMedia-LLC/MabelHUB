9e185524903efe6a032058ad024ca4ac1bcd7404
MabelisYT <mabeliscool1234@gmail.com>
2025-08-07 13:24:20 +0100

    Add files via upload

---
+++ PathHandler-Linux.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+: <<EOF
+
+MabelHUB Executable Search Path Extender Utility - Linux Version
+Copyright MabelMedia LLC.
+
+This Script Extends The Executable Search Path To Allow You To Execute Any Of The Binaries Provided By MabelHUB, Or The Apps It Installs.
+
+EOF
+
+ExtendPath() {
+    export PATH="$1:$PATH"
+}
+
+ExtendPath "$HOME/MabelHUB"
+for AuthorFolder in "$HOME/MabelHUB/Apps"/*; do
+    for AppFolder in "$AuthorFolder"/*; do
+        ExtendPath "$AppFolder"
+    done
+done
